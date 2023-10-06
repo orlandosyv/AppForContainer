5417e3a420456d271e895ef2a25ffd2692160f19
OrlandoYV <sergioyv@gmail.com>
2023-10-05 22:42:23 -0500

    Add project files.

---
+++ ConsoleAppForContainer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World! Start our Counter ....");
+int Numero = 0;
+Console.WriteLine("Would you want to start on a specific number?");
+String Answer = Console.ReadLine();
+if (Answer != null) 
+{
+    if (Answer.ToLower() == "yes") {
+        Console.WriteLine("Write starting number lower than 50");
+        String StartingNumberStr = Console.ReadLine();
+        Numero = Convert.ToInt32(StartingNumberStr);        
+    }
+    else { Console.WriteLine("The Counter Start now!"); }
+    ;
+}
+
+while (Numero < 50) {
+    Numero++;
+    Console.WriteLine(Numero);
+}
+Console.WriteLine("The Counter has finished");
